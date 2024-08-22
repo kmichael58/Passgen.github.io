b8aeb3da421f7c57ad7813838c6de909c6a6ab14
kmichael58 <164901685+kmichael58@users.noreply.github.com>
2024-08-22 13:39:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var password = document.getElementById("password");
+function genPassword() {
+  var chars ="0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passLength = 8;
+  var password = "";
+  for (var i = 0; i < passLength; i++){
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById("password").value = password;
+}
+function copyPassword(){
+  var copyText = document.getElementById("password");
+  copyText.select();
+  document.execCommand("copy");
+}
